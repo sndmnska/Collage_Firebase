af7d33b0810a40db969ba198b6179af1ebddfc25
dan <email@email.com>
2023-04-13 18:55:20 -0500

    Functionality: Upload to Firebase Storage

--- app/src/main/java/com/smeiskaudio/collage/MainActivity.kt
+++ app/src/main/java/com/smeiskaudio/collage/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.smeiskaudio.collage
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -8,9 +9,14 @@ import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ProgressBar
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.squareup.picasso.*
 import java.io.File
 import java.io.IOException
@@ -23,10 +29,14 @@ private const val TAG = "MAIN_ACTIVITY"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var imageButton1: ImageButton
+    private lateinit var uploadImageFab: FloatingActionButton
+    private lateinit var uploadProgressBar: ProgressBar
     private lateinit var mainView: View
 
     private var newPhotoPath: String? = null
     private var visibleImagePath: String? = null
+    private var imageFileName: String? = null
+    private var photoUri: Uri? = null
 
     private val NEW_PHOTO_PATH_KEY = "com.smeiskaudio.collage.NEW_PHOTO_PATH_KEY"
     private val VISIBLE_IMAGE_PATH_KEY = "com.smeiskaudio.collage.VISIBLE_IMAGE_PATH_KEY"
@@ -35,6 +45,9 @@ class MainActivity : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             result -> handleImage(result)
         }
+
+    // Firebase storage
+    private val storage = Firebase.storage
 
 
     private fun handleImage(result: ActivityResult) {
@@ -60,9 +73,38 @@ class MainActivity : AppCompatActivity() {
 
         mainView = findViewById(R.id.content)
 
+        uploadProgressBar = findViewById(R.id.uploadProgressBar)
+
+        uploadImageFab = findViewById(R.id.upload_image_button)
+        uploadImageFab.setOnClickListener {
+            uploadImage()
+        }
+
         imageButton1 = findViewById(R.id.imageButton1)
         imageButton1.setOnClickListener {
             takePicture()
+        }
+    }
+
+    private fun uploadImage() {
+        if (photoUri != null && imageFileName != null) {
+            val imageStorageRootReference = storage.reference
+            val imageCollectionReference = imageStorageRootReference.child("images")
+            val imageFileReference = imageCollectionReference.child(imageFileName!!)
+            imageFileReference.putFile(photoUri!!)
+                .addOnCompleteListener {
+                    Snackbar.make(mainView, "Image uploaded",Snackbar.LENGTH_LONG).show()
+            }
+                .addOnFailureListener { error ->
+                    Snackbar.make(mainView,"Error uploading image", Snackbar.LENGTH_LONG).show()
+                    Log.e(TAG, "Error uploading image -- $error")
+                }
+        } else {
+            Snackbar.make(
+                mainView,
+                "Take a picture first",
+                Snackbar.LENGTH_SHORT
+            ).show()
         }
     }
 
@@ -76,8 +118,8 @@ class MainActivity : AppCompatActivity() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         val (photoFile, photoFilePath) = createImageFile()
         if (photoFile != null) {
-            newPhotoPath = photoFilePath
-            val photoUri = FileProvider.getUriForFile( // reference work to tell camera app where to save file
+            newPhotoPath = photoFilePath // send to global variables instead of variables inside the function.
+            photoUri = FileProvider.getUriForFile( // reference work to tell camera app where to save file
                 this,
                 "com.smeiskaudio.collage.fileprovider",
                 photoFile
@@ -91,9 +133,9 @@ class MainActivity : AppCompatActivity() {
     private fun createImageFile(): Pair<File?, String?> {
         return try {
             val dateTime = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-            val imageFileName = "COLLAGE_$dateTime"
+            imageFileName = "COLLAGE_$dateTime" // non-null asserted here, because a value happens.
             val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-            val file = File.createTempFile(imageFileName, ".jpg", storageDir)
+            val file = File.createTempFile(imageFileName!!, ".jpg", storageDir)
             val filePath = file.absolutePath
             file to filePath
         } catch (ex: IOException) {
